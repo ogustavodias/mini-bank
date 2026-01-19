645ee4e16ee212024e02cc9c7386090803291274
ogustavodias <gustavoalvesdiaz08@gmail.com>
2026-01-19 15:04:08 -0300

    feature: implementação mínima do resilience4j

--- src/main/java/com/mini/bank/clients/transfer/AuthorizeClient.java
+++ src/main/java/com/mini/bank/clients/transfer/AuthorizeClient.java
@@ -3,9 +3,12 @@ package com.mini.bank.clients.transfer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
 @FeignClient(name = "authorizeClient", url = "https://util.devi.tools/api/v2", configuration = AuthorizeConfig.class)
 public interface AuthorizeClient {
 
    @GetMapping("/authorize")
+   @Retry(name = "authorizeClient")
    Object authorizeTransfer();
 }
--- src/main/java/com/mini/bank/clients/transfer/NotifyClient.java
+++ src/main/java/com/mini/bank/clients/transfer/NotifyClient.java
@@ -3,10 +3,13 @@ package com.mini.bank.clients.transfer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
 @FeignClient(name = "notifyClient", url = "https://util.devi.tools/api/v1", configuration = NotifyConfig.class)
 public interface NotifyClient {
 
    @PostMapping("/notify")
+   @Retry(name = "notifyClient")
    Object notifyTransfer();
 
 }
